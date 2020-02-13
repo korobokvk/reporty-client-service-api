ce1712199dfaafa2c952e8ca9d6b609b4fdf06e2
Maksym Zakshevskyi <mzakshevskyi@lohika.com>
2020-02-13 02:35:30 +0200

    refactoring

--- src/grpc.interface.ts
+++ src/grpc.interface.ts
@@ -1,8 +1,8 @@
 import { Observable } from 'rxjs'
 
 export interface IUser {
-  userAuth?(credentials: Observable<ICredentials>): Observable<boolean>
-  isAuthUser?(JWT: IJWT): Observable<boolean>
+  userAuth(credentials: Observable<ICredentials>): Observable<boolean>
+  isAuthUser(JWT: IJWT): Observable<boolean>
 }
 interface IJWT {
   JWT: string
